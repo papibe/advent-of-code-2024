ab3493783fa95dad945d29eedb09e3b3d9b42ebe
Pablo Piaggio <Pablo.Piaggio@gmail.com>
2024-12-10 21:39:38 -0600

    Improve part 2

--- python/day09/part2.py
+++ python/day09/part2.py
@@ -1,91 +1,73 @@
-import re
-from collections import deque, defaultdict
-from dataclasses import dataclass
-from typing import Deque, Dict, List, Match, Optional, Set, Tuple, NamedTuple
-from copy import deepcopy
+from collections import namedtuple
+from typing import Dict, List, Tuple
 
-class File(NamedTuple):
-    index: int
-    size: int
+File = namedtuple("File", ["index", "size"])
+# File = Tuple[int, int]
+Files = Dict[int, File]
+Space = List[File]
 
 
-def parse(filename: str) -> List[str]:
+def parse(filename: str) -> Tuple[Files, Space, int]:
     with open(filename, "r") as fp:
-        data: List[str] = fp.read().strip()
+        data: str = fp.read().strip()
 
     file_id = 0
-    content: Dict[int, File] = {}
-    space: Dict[int, int] = {}
+    files: Files = {}
+    space: Space = []
     disk_pointer = 0
 
     for index in range(0, len(data), 2):
         # files
         blocks = int(data[index])
-        content[file_id] = File(disk_pointer, blocks)
+        files[file_id] = File(disk_pointer, blocks)
         disk_pointer += blocks
 
         # spaces
         if index + 1 < len(data):
             space_size = int(data[index + 1])
-            space[disk_pointer] = space_size
+            space.append(File(disk_pointer, space_size))
             disk_pointer += space_size
 
         file_id += 1
 
-    return content, space, file_id - 1
+    return files, space, file_id - 1
 
 
-
-
-def solve(files, spaces, latest_content) -> int:
-
+def solve(files: Files, spaces: Space, latest_content: int) -> int:
     for content_id in range(latest_content, -1, -1):
 
-        original_index = files[content_id].index
-        size_to_be_moved = files[content_id].size
+        original_index: int = files[content_id].index
+        size_to_be_moved: int = files[content_id].size
 
-        spaces_indexes = sorted(spaces.keys())
-
-        for index in spaces_indexes:
-
+        for i, (index, space_size) in enumerate(spaces):
             if index >= original_index:
-                continue
-            space_size = spaces[index]
+                break
 
             if space_size >= size_to_be_moved:
 
-                spaces[original_index] = size_to_be_moved
+                # do not create free space in the place of the move file.
+                # it does nothing
 
-                files[content_id] = (index, size_to_be_moved)
+                files[content_id] = File(index, size_to_be_moved)
 
                 new_space_size = space_size - size_to_be_moved
                 new_space_index = index + size_to_be_moved
-                del spaces[index]
 
                 if new_space_size > 0:
-                    spaces[new_space_index] = new_space_size
+                    spaces[i] = File(new_space_index, new_space_size)
+                else:
+                    spaces.pop(i)
 
                 break
 
-    total = 0
-    content_indexes = {}
-    for k, v in files.items():
-        content_indexes[v[0]] = (k, v[1])
-
-    disk_index = 0
-    for index, item in content_indexes.items():
-        k, v = item
-        total +=  k * (
-            (index + v - 1) * (index + v) // 2
-            - (index - 1) * index // 2
+    checksum: int = 0
+    for file_id, (index, size) in files.items():
+        checksum += file_id * (
+            (index + size - 1) * (index + size) // 2 - (index - 1) * index // 2
         )
 
+    return checksum
 
-
-    print(f"{total = }")
-    assert total == 6362722604045
-
-    return total
 
 def solution(filename: str) -> int:
     content, space, latest_content = parse(filename)
@@ -93,6 +75,5 @@ def solution(filename: str) -> int:
 
 
 if __name__ == "__main__":
-    # print(solution("./example2.txt"))  # 0
-    # print(solution("./example.txt"))  # 0
+    print(solution("./example1.txt"))  # 2858
     print(solution("./input.txt"))  # 6362722604045
