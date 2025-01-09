b54ae7f649caf7b193efc3c5e9bfc4ba3250679d
Pablo Piaggio <Pablo.Piaggio@gmail.com>
2025-01-09 14:05:05 -0600

    Lint and add type annotations

--- python/day21/part2.py
+++ python/day21/part2.py
@@ -1,60 +1,35 @@
-import re
-from collections import deque, defaultdict
+from collections import defaultdict, deque
 from itertools import product
-from dataclasses import dataclass
-from typing import Deque, Dict, List, Match, Optional, Set, Tuple
+from typing import DefaultDict, Deque, Dict, List, Tuple
+
+Pad = List[str]
+Cost = DefaultDict[Tuple[int, int], int]
+Queue = Deque[Tuple[int, int, int, str]]
+Paths = Dict[str, List[str]]
+Memo = Dict[Tuple[str, str, int], int]
+Sequences = List[str]
 
 
-def parse(filename: str) -> str:
+def parse(filename: str) -> List[str]:
     with open(filename, "r") as fp:
-        data: str = fp.read().splitlines()
+        data: List[str] = fp.read().splitlines()
 
     return data
 
-NUM_KEYPAD_MAP = {
-    "7": (0, 0),
-    "8": (0, 1),
-    "9": (0, 2),
 
-    "4": (1, 0),
-    "5": (1, 1),
-    "6": (1, 2),
+NUM_KEYPAD = ["789", "456", "123", "#0A"]
 
-    "1": (2, 0),
-    "2": (2, 1),
-    "3": (2, 2),
-
-    "#": (3, 0),
-    "0": (3, 1),
-    "A": (3, 2),
-}
-
-NUM_KEYPAD = [
-    "789",
-    "456",
-    "123",
-    "#0A"
-]
-
-DIR_KEYPAD_MAP = {
-    "#": (0, 0),
-    "^": (0, 1),
-    "A": (0, 2),
-
-    "<": (1, 0),
-    "v": (1, 1),
-    ">": (1, 2),
-}
 
 DIR_KEYPAD = [
     "#^A",
     "<v>",
 ]
 
-def get_paths(start_row, start_col, pad):
-    ways = {}
-    costs = defaultdict(lambda: float("inf"))
-    queue = deque([(start_row, start_col, 0, "")])
+
+def get_paths(start_row: int, start_col: int, pad: Pad) -> Paths:
+    ways: Paths = {}
+    costs: Cost = defaultdict(lambda: float("inf"))  # type: ignore
+    queue: Queue = deque([(start_row, start_col, 0, "")])
     costs[(start_row, start_col)] = 0
     ways[pad[start_row][start_col]] = [""]
 
@@ -64,80 +39,75 @@ def get_paths(start_row, start_col, pad):
             (row + 1, col, "v"),
             (row - 1, col, "^"),
             (row, col + 1, ">"),
-            (row, col - 1, "<")
+            (row, col - 1, "<"),
         ]:
             if 0 <= new_row < len(pad) and 0 <= new_col < len(pad[0]):
                 if pad[new_row][new_col] == "#":
                     continue
-                new_distance = distance + 1
+                new_distance: int = distance + 1
                 if new_distance < costs[(new_row, new_col)]:
-
-                    # print(costs[(new_row, new_col)], new_distance)
                     costs[(new_row, new_col)] = new_distance
                     queue.append((new_row, new_col, new_distance, paths + dir))
 
                     ways[pad[new_row][new_col]] = [paths + dir]
+
                 elif new_distance == costs[(new_row, new_col)]:
                     ways[pad[new_row][new_col]].append(paths + dir)
                     queue.append((new_row, new_col, new_distance, paths + dir))
-                    
+
     return ways
 
 
-def build_numpad_paths(pad):
-    paths = {}
+def build_numpad_paths(pad: Pad) -> Dict[str, Paths]:
+    paths: Dict[str, Paths] = {}
     for row, line in enumerate(pad):
         for col, item in enumerate(line):
             if item != "#":
                 paths[item] = get_paths(row, col, pad)
-                # break
-
-    # for k,v in paths.items():
-    #     print(k, v)
-    #     print()
 
     return paths
 
-def get_num_pad(code, numpad):
 
-    sols = []
-    prev = "A"
+def get_num_pad(code: str, numpad: Dict[str, Paths]) -> List[str]:
+
+    sols: List[List[str]] = []
+    prev: str = "A"
     for char in code:
         local_sol = []
         for path in numpad[prev][char]:
             local_sol.append(path + "A")
         sols.append(local_sol)
         prev = char
-    fsols = product(*sols)
-    otuput = []
+
+    fsols: List[Tuple[str]] = product(*sols)  # type: ignore
+    otuput: List[str] = []
     for sol in fsols:
         otuput.append("".join(sol))
     return otuput
 
 
-memo = {}
+def get_dir_pad(
+    from_: str, to: str, dirpath: Dict[str, Paths], intake_level: int
+) -> int:
 
-def get_dir_pad(from_, to, dirpath, intake_level):
-    
-    def _get_dir_pad(from_, to, level):
+    memo: Memo = {}
+
+    def _get_dir_pad(from_: str, to: str, level: int) -> int:
         if level == 1:
             return len(dirpath[from_][to][0])
 
-        key = (from_, to, level)
+        key: Tuple[str, str, int] = (from_, to, level)
         if key in memo:
             return memo[key]
 
-        best_result = float("inf")
-        # print(f"{dirpath[from_][to] = }")
+        best_result: int = float("inf")  # type: ignore
         for path in dirpath[from_][to]:
-            # print(from_, to, path, level)
             result = 0
             prev = "A"
             for char in path:
                 result += _get_dir_pad(prev, char, level - 1)
                 prev = char
 
-            # print(f"{result = }")
             best_result = min(best_result, result)
 
         memo[key] = best_result
@@ -146,33 +116,21 @@ def get_dir_pad(from_, to, dirpath, intake_level):
     return _get_dir_pad(from_, to, intake_level)
 
 
-
-def solve(data: str, numpad, dirpath) -> int:
+def solve(data: List[str], numpad: Dict[str, Paths], dirpath: Dict[str, Paths]) -> int:
     total_sum: int = 0
-        
+
     for code in data:
- 
+
         sols = get_num_pad(code, numpad)
 
-        best_result = float("inf")
+        best_result: int = float("inf")  # type: ignore
         for sol in sols:
-            print(sol)
             result = 0
             prev = "A"
-            counter = 0
             for char in sol:
-                r = get_dir_pad(prev, char, dirpath, 25)
-                # print("  ", prev, char, r)
-                result += r
+                result += get_dir_pad(prev, char, dirpath, 25)
                 prev = char
-                # counter += 1
-                # if counter == 2:
-                #     break
-            # print("result", result)
             best_result = min(best_result, result)
-            # break
-
-        print(code, best_result)
 
         total_sum += best_result * int(code[:-1])
 
@@ -181,24 +139,18 @@ def solve(data: str, numpad, dirpath) -> int:
 
 def solution(filename: str) -> int:
     numpad_paths = build_numpad_paths(NUM_KEYPAD)
-    # print(numpad_paths)
-    # return 0
     dirpad_paths = build_numpad_paths(DIR_KEYPAD)
-    dirpaths = {}
+
+    # patch paths because we have 2 diff functions for pads
     for k, v in dirpad_paths.items():
         for k2, paths in v.items():
             new_paths = []
             for path in paths:
                 new_paths.append(path + "A")
-
             v[k2] = new_paths
 
-    # for k, v in dirpad_paths.items():
-    #     print(k, v)
-
-    data: str = parse(filename)
+    data: List[str] = parse(filename)
     return solve(data, numpad_paths, dirpad_paths)
-    # return 0
 
 
 if __name__ == "__main__":
