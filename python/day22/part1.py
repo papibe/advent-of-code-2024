eb6b135268cb051e100e8d6425ba069defc8d54e
Pablo Piaggio <Pablo.Piaggio@gmail.com>
2025-01-10 09:51:54 -0600

    Lint and add type anontations for part 1

--- python/day22/part1.py
+++ python/day22/part1.py
@@ -1,32 +1,31 @@
-import re
-from typing import List, Tuple
+from typing import List
 
 
-def parse(filename: str) -> str:
+def parse(filename: str) -> List[int]:
     with open(filename, "r") as fp:
-        data: str = fp.read().splitlines()
+        data: List[str] = fp.read().splitlines()
 
     numbers: List[int] = []
     for line in data:
         numbers.append(int(line))
 
-    # print(numbers)
-
     return numbers
 
+
 def pseudo_random(n: int) -> int:
-    def round(n: int):
+
+    def pseudo_round(n: int) -> int:
         n = (n ^ (n * 64)) % 16777216
         n = (n ^ (n // 32)) % 16777216
         n = (n ^ (n * 2048)) % 16777216
         return n
 
     for _ in range(2000):
-        n = round(n)
+        n = pseudo_round(n)
     return n
 
 
-def solve(numbers: str) -> int:
+def solve(numbers: List[int]) -> int:
     total_sum: int = 0
 
     for number in numbers:
@@ -37,7 +36,7 @@ def solve(numbers: str) -> int:
 
 def solution(filename: str) -> int:
 
-    data: str = parse(filename)
+    data: List[int] = parse(filename)
     return solve(data)
 
 
