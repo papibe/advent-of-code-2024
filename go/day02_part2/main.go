98992a70ea2fe2c158c184b6a8afcf9f3cf98846
Pablo Piaggio <Pablo.Piaggio@gmail.com>
2025-01-13 22:05:15 -0600

    Add day 2 on go both parts

---
+++ go/day02_part2/main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func parse(filename string) [][]int {
+	data, err := os.ReadFile(filename)
+
+	if err != nil {
+		panic("file error")
+	}
+	reports := [][]int{}
+
+	lines := strings.Split(strings.Trim(string(data), "\n"), "\n")
+
+	for _, line := range lines {
+		report := []int{}
+		for _, num_str := range strings.Split(line, " ") {
+			num, _ := strconv.Atoi(num_str)
+			report = append(report, num)
+		}
+		reports = append(reports, report)
+	}
+	return reports
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+func is_valid(report []int) bool {
+	if report[0] == report[1] {
+		return false
+	}
+
+	var decreasing bool
+	if report[0] < report[1] {
+		decreasing = true
+	} else {
+		decreasing = false
+	}
+
+	for i := 0; i < len(report)-1; i++ {
+		current := report[i]
+		next := report[i+1]
+
+		if decreasing && current > next {
+			return false
+		}
+		if !decreasing && current < next {
+			return false
+		}
+		abs_diff := abs(current - next)
+		if !(1 <= abs_diff && abs_diff <= 3) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func solve(reports [][]int) int {
+	valid_reports := 0
+
+	for _, report := range reports {
+
+		for index := 0; index < len(report); index++ {
+			simple_report := make([]int, len(report))
+			copy(simple_report, report)
+			simple_report = append(simple_report[:index], simple_report[index+1:]...)
+
+			if is_valid(simple_report) {
+				valid_reports++
+				break
+			}
+		}
+	}
+	return valid_reports
+}
+
+func solution(filename string) int {
+	reports := parse(filename)
+	return solve(reports)
+}
+
+func main() {
+	fmt.Println(solution("./example.txt")) // 4
+	fmt.Println(solution("./input.txt"))   // 528
+}
